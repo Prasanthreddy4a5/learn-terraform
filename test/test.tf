3b7e3c4e94251cdefe6cc732a791fcab947a23d3
prasanth4a5 <ummadi1994@gmail.com>
2023-12-23 15:41:45 +0530

    Learn Terraform

--- test/test.tf
+++ test/test.tf
@@ -1,46 +1,7 @@
-## Map Variable, Map of Maps
-#variable "fruit_stock_with_price" {
-#  default = {
-#    apple = {
-#      stock = 200
-#      price = 3
-#    }
-#    banana = {
-#      stock = 400
-#      price = 1
-#    }
-#  }
-#}
-#
-#
-#output "fruit_stock" {
-#  value = var.fruit_stock_with_price["apple"].stock
-#}
-#output "fruit_price" {
-#  value = var.fruit_stock_with_price["apple"].price
-#}
-
-
-# Map Variable , Plain
-#variable "fruit_stock" {
-#  default = {
-#    apple = 100
-#    banana = 200
-#  }
-#}
-
-variable "Salaar" {
-
-  default = {
-    Prabhas    = "Praboss"
-    Collection = 200
-  }
+variable "fruits" {
+  default = [ "apple", "banana"]
 }
 
-output "Sallar" {
-  value = var.Salaar["Prabhas"]
-}
-
-output "Sallar_Collection" {
-  value = var.Salaar["Collection"]
+output "fruits" {
+  value = element(var.fruits,1)
 }
