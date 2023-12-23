2902797e7bfd89e49ac7ab08e4941e27a486de4a
prasanth4a5 <ummadi1994@gmail.com>
2023-12-23 14:34:11 +0530

    Learn Terraform

---
+++ test/test.tf
@@ -0,0 +1,18 @@
+# Map Variable, Map of Maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 200
+      price = 3
+    }
+    banana = {
+      stock = 400
+      price = 1
+    }
+  }
+}
+
+
+output "fruit_stock" {
+  value = "var.fruit_stock_with_price[apple].price"
+}
