5fd68404f7aa5d88dfbc9472f2a4e4769f288316
prasanth4a5 <ummadi1994@gmail.com>
2023-12-23 15:43:41 +0530

    Learn Terraform

--- test/test.tf
+++ test/test.tf
@@ -1,7 +1,7 @@
 variable "fruits" {
-  default = [ "apple", "banana"]
+  default = [ "apple", "banana", pineapple]
 }
 
 output "fruits" {
-  value = element(var.fruits,1)
+  value = element(var.fruits,2)
 }
