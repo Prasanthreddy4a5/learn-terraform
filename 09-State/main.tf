2fc2ac6f4a4033765b6782ae6ac62a58ccc0bd4c
prasanth4a5 <ummadi1994@gmail.com>
2024-01-26 12:15:09 +0530

    test

--- 09-State/main.tf
+++ 09-State/main.tf
@@ -14,18 +14,18 @@ resource "aws_s3_bucket" "prash74_bucket" {
 }
 
 
-#terraform {
-#  backend "s3" {
-#    bucket = "tf-state-prash74"
-#    key    = "09-state/terraform.tfstate"
-#    region = "us-east-1"
-#  }
-#}
-#
-#variable "test" {
-#  default = "Hello state file"
-#}
-#
-#output "test" {
-#  value = var.test
-#}
+terraform {
+  backend "s3" {
+    bucket = "tf-state-prash74"
+    key    = "09-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "test" {
+  default = "Hello state file"
+}
+
+output "test" {
+  value = var.test
+}
